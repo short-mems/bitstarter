c2047b9839b9c29400bff62d8a67ccd22c2c5d2e
Timofey Chernousov <short.mems@gmail.com>
2013-07-07 12:01:22 +0000

    moved google analytics to index.html

--- web.js
+++ web.js
@@ -4,8 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var fs_ga = require('fs');
-  response.send(fs.readFileSync('index.html').toString()+fs_ga.readFileSync('ga.js').toString());
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
